e0e8b24344e2f54aa381c2404ac9032216d842db
Lennart Oelschläger <lennart.oelschlaeger@uni-bielefeld.de>
2023-12-21 11:10:02 +0000

    version 0.1.1

--- R/zzz.R
+++
@@ -1,8 +0,0 @@
-.onAttach = function(lib, pkg) {
-  # startup message
-  msg = paste0(
-    "Thanks for installing vntrs ", packageVersion("vntrs"), ", happy search!\n",
-    "Type 'citation(\"vntrs\")' for citing the package in publications.")
-  packageStartupMessage(msg)
-  invisible()
-}
